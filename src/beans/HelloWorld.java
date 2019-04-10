565307fb6b057156e1e9ba2c938543acb2b2dd6d
Mohideen Abdul Katheer M <abdulkatheerm@gmail.com>
2019-04-10 09:32:38 +0530

    Final commit

---
+++ src/beans/HelloWorld.java
@@ -0,0 +1,8 @@
+package beans;
+
+public class HelloWorld {
+    public void sayHello() {
+        System.out.println("Hello World, This is Spring Framework, You're using Intellij IDEA!! That's nice to hear!!!");
+    }
+
+}
